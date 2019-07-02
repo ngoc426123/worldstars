8667940bf0a622aff5101f0a4a537d892d2a30b9
ngoc426123 <48208982+ngoc426123@users.noreply.github.com>
2019-07-02 22:37:01 +0700

    22h36 02/07/2019

--- js/main/main.js
+++ js/main/main.js
@@ -1,3 +1,42 @@
 $(document).ready(function(){
-	
+	$("#slideHome").slick({
+		fade:true,
+		dots:true,
+	});
+	$("#slideSinger").slick({
+		slidesToShow : 3,
+		autoplay:true,
+		responsive: [
+            {
+                breakpoint:767,
+                settings: {
+                    slidesToShow:2,
+                }
+            },
+            {
+                breakpoint:365,
+                settings: {
+                    slidesToShow:1,
+                }
+            }
+        ]
+	});
+	$("#slideVideo").slick({
+		slidesToShow : 3,
+		autoplay:true,
+		responsive: [
+            {
+                breakpoint:767,
+                settings: {
+                    slidesToShow:2,
+                }
+            },
+            {
+                breakpoint:365,
+                settings: {
+                    slidesToShow:1,
+                }
+            }
+        ]
+	});
 });
