571883d6cf13e16061ea664d444521750f9c3135
ngoc426123 <48208982+ngoc426123@users.noreply.github.com>
2019-07-27 22:20:31 +0700

    22h20 27/07/2019

---
+++ js/news/news.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	// NEWS SLIDE
+	$("#wst-news-slider").slick({
+        arrows:false,
+        dots:true,
+        fade:true,
+        autoplay:true,
+    });
+});
--- js/singer/singer.js
+++ js/singer/singer.js
@@ -1,64 +1,30 @@
 $(document).ready(function(){
-	$("#slideThumbnail").slick({
-		slideToShow : 3,
-		variableWidth: true,
-		autoplay:true,
-		centerMode: true,
-  		centerPadding: '0px',
-  		responsive: [
-            {
-                breakpoint:600,
-                settings: {
-                    slidesToShow:1,
-                    variableWidth: false,
-                    centerMode: false,
-                }
-            }
-        ]
+	$(".the-thumbnail").slick({
+		fade:true,
+        autoplay:true,
 	});
-	$(".productColor ul li a").click(function(){
-		$(this).parent("li").siblings().removeClass("active");
-        if(!$(this).parent("li").hasClass("active")){
-            $(this).parent("li").addClass("active");
-        }
-        else{
-            $(this).parent("li").removeClass("active");
-        }
-        return false;
-    });
-    $("#slideOther").slick({
-    	slidesToShow : 3,
-    	autoplay:true,
-    	responsive: [
-            {
-                breakpoint:991,
-                settings: {
-                    slidesToShow:2,
-                }
-            },
-            {
-                breakpoint:600,
-                settings: {
-                    slidesToShow:1,
-                }
-            }
-        ]
-    });
-    $(".linkAll a").click(function(){
-        $.ajax({
-            url:'4_xe_moi_nhap_more_1.html',
-            type:'post',
-            async:true,
-            beforeSend:function(){
-                $("html").addClass("openmenu2 loading");
-            },
-            success:function(e){
-                setTimeout(function(){
-                    $("#mvb-product .row").append(e);
-                    $("html").removeClass("openmenu2 loading");
-                },2000);
-            }
-        });
-        return false;
-    });
+	$(".slideOther").slick({
+		slidesToShow : 4,
+		autoplay:true,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 520,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 360,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
+	});
 });
